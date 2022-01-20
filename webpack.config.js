2dae740459279ad6b3cb54c3b3d73a7a531ec9f1
miaoyqa@yonyou.com <miaoyqa@yonyou.com>
2022-01-21 04:22:54 +0800

    complete stage1

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,26 @@
-const path = require('path')
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 module.exports = {
-  mode:'production',
-  entry:{
-    index: './lib/index.tsx'
+  mode: "production",
+  entry: {
+    index: "./lib/index.tsx"
   },
-  output:{
-    path: path.resolve(__dirname,'dist/lib'),
-    library: 'LUI',
-    libraryTarget:'umd',
+  output: {
+    path: path.resolve(__dirname, "dist/lib"),
+    library: "LUI",
+    libraryTarget: "umd"
   },
-  module:{
-    rules:[
+  module: {
+    rules: [
       {
-        test:/\.tsx?$/,
-        loader: 'awesome-typescript-loader',
+        test: /\.tsx?$/,
+        loader: "awesome-typescript-loader"
       }
     ]
-  }
-}
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "index.html"
+    })
+  ]
+};
